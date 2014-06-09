48c2b63d4f9fd8c4a28275a6908f2dd3722eb665
rand0m <rand0m@202c2015-9df6-4aa8-88a8-d54e55967ac9>
2014-06-09 21:51:36 +0000

    Resources updated

---
+++ src/main/webapp/res/js/app.js
@@ -0,0 +1,59 @@
+// Page effects initialization
+$(document).ready(function() {
+
+	// Title animation
+	$('.htitl').animate({left: "+=1000"}, 1000);
+	
+	// To top button
+	$().UItoTop({ easingType: 'easeOutQuart' });
+	$('.backtotop').click(function(){
+		$('html, body').animate({scrollTop:0}, 'slow');
+	});
+
+	// Flexislider
+	$('.flexslider').flexslider({
+		animation: "slide",
+		controlNav: false,
+		prevText: "",
+		nextText: ""
+	});
+	
+	// Scrolling
+	$(window).on("scroll resize", function (){
+		var window_top = $(window).scrollTop();
+		var projects_top = $('a[name="projects"]').offset().top - 10;
+		var comand_top = $('a[name="comand"]').offset().top - 10;
+		var quality_top = $('a[name="quality"]').offset().top - 300;
+		if (window_top > quality_top) {
+			$("ul.nav li").removeClass("active");
+			$('a#quality').parent().addClass("active");
+		}
+		else if (window_top > comand_top) {
+			$("ul.nav li").removeClass("active");
+			$('a#comand').parent().addClass("active");
+		}
+		else if (window_top > projects_top) {
+			$("ul.nav li").removeClass("active");
+			$('a#projects').parent().addClass("active");
+		}
+		else {
+			$("ul.nav li").removeClass("active");
+			$('a#nas').parent().addClass("active");
+		}
+	});
+
+	$('.parallax.top').scrolly();
+	$('.parallax.bkg').scrolly({bgParallax: true});
+
+
+	$('.menuItem').click(function(event) {
+		event.stopPropagation();
+		var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+		var $this = $(this);
+		var target = $this.attr('data-parent');
+		if (width < 769) {
+			$(target).collapse('toggle');			
+		}
+	});
+
+});
