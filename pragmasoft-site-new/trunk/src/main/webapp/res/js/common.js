9a0bc4d2a212a745b0e568a76130a7f7c6cf763d
rand0m <rand0m@202c2015-9df6-4aa8-88a8-d54e55967ac9>
2014-06-09 21:51:36 +0000

    Resources updated

--- pragmasoft-site-new/trunk/src/main/webapp/res/js/common.js
+++
@@ -1,9 +0,0 @@
-function togglePanel(panelId, cookieName) {
-    if(eltHasClass($(panelId), 'mode_expand')) {
-        eraseCookie(cookieName); 
-        $(panelId).className='border_panel mode_min';
-    } else { 
-        createCookie(cookieName,'any_value', '');
-        $(panelId).className='border_panel mode_expand';
-	}
-}
