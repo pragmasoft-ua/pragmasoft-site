2d8279a88147d8a700bea007ec76f5309ca62745
rand0m <rand0m@202c2015-9df6-4aa8-88a8-d54e55967ac9>
2014-06-26 22:38:00 +0000

    Disabling scrolling algorithm updated.

--- src/main/webapp/res/js/app.js
+++ src/main/webapp/res/js/app.js
@@ -18,33 +18,55 @@ $(document).ready(function() {
 		nextText: ""
 	});
 
-	var disableScrolling = function() {
-		$('html, body').css({
-    		'overflow': 'hidden',
-    		'height': '100%'
-		});
+	var UserScrollDisabler = function() {	    
+	    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+	    // left: 37, up: 38, right: 39, down: 40
+	    this.scrollEventKeys = [32, 33, 34, 35, 36, 37, 38, 39, 40];
+	    this.$window = $(window);
+	    this.$document = $(document);	    
+	};
+	UserScrollDisabler.prototype = {	    
+	    disable_scrolling : function() {
+	        var t = this;
+	        t.$window.on("mousewheel.UserScrollDisabler DOMMouseScroll.UserScrollDisabler", this._handleWheel);
+	        t.$document.on("mousewheel.UserScrollDisabler touchmove.UserScrollDisabler", this._handleWheel);
+	        t.$document.on("keydown.UserScrollDisabler", function(event) {
+	            t._handleKeydown.call(t, event);
+	        });
+	    },	    
+	    enable_scrolling : function() {
+	        var t = this;
+	        t.$window.off(".UserScrollDisabler");
+	        t.$document.off(".UserScrollDisabler");
+	    },	    
+	    _handleKeydown : function(event) {
+	        for (var i = 0; i < this.scrollEventKeys.length; i++) {
+	            if (event.keyCode === this.scrollEventKeys[i]) {
+	                event.preventDefault();
+	                return;
+	            }
+	        }
+	    },	    
+	    _handleWheel : function(event) {
+	        event.preventDefault();
+	    }	    
 	};
 
-	var enableScrolling = function() {
-		$('html, body').css({
-		    'overflow': 'auto',
-		    'height': 'auto'
-		});
-	};
+	var disabler = new UserScrollDisabler();
 
 	// Disable scrolling while menu is opened in mobile version
 	$('button.navbar-toggle').click(function (event) {
 		if ($('div.tnavbar').hasClass('in')) {
-			enableScrolling();
+			disabler.enable_scrolling();
 		} else {
-			disableScrolling();
+			disabler.disable_scrolling();
 		}
 	});
 
 	// Hide menu button on click in mobile mode
 	$('.menuItem').click(function(event) {
 		if ($('div.tnavbar').hasClass('in')) {
-			enableScrolling();
+			disabler.enable_scrolling();
 		}
 		event.stopPropagation();
 		var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
