34d618a234d684a97119fb7588c458331951e2c0
tarasovserg <tarasovserg@202c2015-9df6-4aa8-88a8-d54e55967ac9>
2014-07-14 08:52:22 +0000

    Added success message for the email

--- pragmasoft-site-new/trunk/src/main/java/ua/com/pragmasoft/web/ContactUsServlet.java
+++ pragmasoft-site-new/trunk/src/main/java/ua/com/pragmasoft/web/ContactUsServlet.java
@@ -6,20 +6,34 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ContactUsServlet extends HttpServlet {
 
-	private static final long serialVersionUID = -5714177758356176506L;
+	private static final long serialVersionUID = -5714177759316176506L;
 	private static final Logger log = LoggerFactory.getLogger(ContactUsServlet.class);
 	
+	private static final String PATH_TO_CONTACT_US_EN = "/pages/orderProject_en.ftl";
+	private static final String PATH_TO_CONTACT_US_RU = "/pages/orderProject_ru.ftl";
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		log.debug("Request URI: {}", request.getRequestURI());
 		
+		HttpSession session = ((HttpServletRequest) request).getSession();
+		String path;
+
+		if (session.getAttribute(Constants.LANGUAGE).equals("ru")) {
+			path = PATH_TO_CONTACT_US_RU;
+		} else {
+			path = PATH_TO_CONTACT_US_EN;
+		}
+		
+		request.setAttribute("path", path);
 		request.getRequestDispatcher("/pages/contactUs.ftl").forward(request, response);
 	}
 }
